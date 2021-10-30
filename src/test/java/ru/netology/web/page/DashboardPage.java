70ed13410e8a613c1f0e4625b177c5869a833801
iva1111 <mvg75@mail.ru>
2021-10-30 13:29:35 +0500

    initial commit

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,49 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+  private SelenideElement topUpFirstCardButton = $("[class=button__content]");
+  private SelenideElement topUpSecondCardButton = $$("[class=button__content]").get(1);
+  private ElementsCollection cards = $$(".list__item");
+  private final String balanceStart = "баланс: ";
+  private final String balanceFinish = " р.";
+
+  private SelenideElement heading = $("[data-test-id=dashboard]");
+
+  public DashboardPage() {
+    heading.shouldBe(visible);
+  }
+
+  public int getFirstCardBalance() {
+    val text = cards.first().text();
+    return extractBalance(text);
+  }
+
+  public int getSecondCardBalance() {
+    val text = cards.last().text();
+    return extractBalance(text);
+  }
+
+  private int extractBalance(String text) {
+    val start = text.indexOf(balanceStart);
+    val finish = text.indexOf(balanceFinish);
+    val value = text.substring(start + balanceStart.length(), finish);
+    return Integer.parseInt(value);
+  }
+
+  public ReplenishCards replenishFirst() {
+    topUpFirstCardButton.click();
+    return new ReplenishCards();
+  }
+
+  public ReplenishCards replenishSecond() {
+    topUpSecondCardButton.click();
+    return new ReplenishCards();
+  }
+}
